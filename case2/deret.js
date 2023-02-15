1fdb334d401b2643171e91147732c95ae343f86c
mahyudin akbar <mahyudinakbar10@gmail.com>
2023-02-15 20:58:37 +0700

    case 2 done

---
+++ case2/deret.js
@@ -0,0 +1,17 @@
+function deretKaskus(n) {
+    //write code here
+    let arr = []
+    for (i = 3; i <= n * 3; i+= 3){
+        if((i % 5 == 0) && (i % 6 == 0)){
+            arr.push('KASKUS')
+        }else if (i % 5 == 0) {
+            arr.push("KAS")
+        }else if(i % 6 == 0){
+            arr.push('KUS')
+        } else {
+            arr.push(i);
+        }
+    }
+    return arr;
+}
+console.log(deretKaskus(10));
