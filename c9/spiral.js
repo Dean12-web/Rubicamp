563a81ead2c105ea5326fa0c43e914cfeae8560e
mahyudin akbar <mahyudinakbar10@gmail.com>
2023-02-28 16:23:35 +0700

    C9 done again

--- c9/spiral.js
+++ c9/spiral.js
@@ -48,40 +48,40 @@ console.log(spiral(5));
 console.log(spiral(6));
 console.log(spiral(7));
 
-var spiralOrder = function (matrix) {
-    const result = [];
-    let left = 0;
-    let top = 0;
-    let right = matrix[0].length - 1;
-    let bottom = matrix.length - 1;
-    let direction = 'right';
-    while (left <= right && top <= bottom) {
-        if (direction === 'right') {
-            for (let i = left; i <= right; i += 1) {
-                result.push(matrix[top][i]);
-            }
-            top += 1;
-            direction = 'down';
-        } else if (direction === 'down') {
-            for (let i = top; i <= bottom; i += 1) {
-                result.push(matrix[i][right]);
-            }
-            right -= 1;
-            direction = 'left';
-        } else if (direction === 'left') {
-            for (let i = right; i >= left; i -= 1) {
-                result.push(matrix[bottom][i]);
-            }
-            bottom -= 1;
-            direction = 'up';
-        } else if (direction === 'up') {
-            for (let i = bottom; i >= top; i -= 1) {
-                result.push(matrix[i][left]);
-            }
-            left += 1;
-            direction = 'right'
-        }
-    }
-    return result
-}
+// var spiralOrder = function (matrix) {
+//     const result = [];
+//     let left = 0;
+//     let top = 0;
+//     let right = matrix[0].length - 1;
+//     let bottom = matrix.length - 1;
+//     let direction = 'right';
+//     while (left <= right && top <= bottom) {
+//         if (direction === 'right') {
+//             for (let i = left; i <= right; i += 1) {
+//                 result.push(matrix[top][i]);
+//             }
+//             top += 1;
+//             direction = 'down';
+//         } else if (direction === 'down') {
+//             for (let i = top; i <= bottom; i += 1) {
+//                 result.push(matrix[i][right]);
+//             }
+//             right -= 1;
+//             direction = 'left';
+//         } else if (direction === 'left') {
+//             for (let i = right; i >= left; i -= 1) {
+//                 result.push(matrix[bottom][i]);
+//             }
+//             bottom -= 1;
+//             direction = 'up';
+//         } else if (direction === 'up') {
+//             for (let i = bottom; i >= top; i -= 1) {
+//                 result.push(matrix[i][left]);
+//             }
+//             left += 1;
+//             direction = 'right'
+//         }
+//     }
+//     return result
+// }
 // console.log(spiralOrder([[0,1,2,3,4],[5,6,7,8,9],[10,11,12,13,14],[15,16,17,18,19],[20,21,22,23,24]]));
