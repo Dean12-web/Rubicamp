c201e9424cc231a388a1ab17f7f10012860000ef
mahyudin akbar <mahyudinakbar10@gmail.com>
2023-03-01 19:34:35 +0700

    C11 done

---
+++ C11/tebakkata.js
@@ -0,0 +1,36 @@
+const filesystem = require('node:fs')
+const readline = require('node:readline');
+
+console.log('Selamat datang di permainan Tebak kata, silahkan isi dengan jawaban yang benar ya! \n')
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'Tebakan:  ',
+});
+filesystem.readFile('data.json', (err, rawdata) => {
+    if (err) throw err;
+    const data = JSON.parse(rawdata);
+    let index = 0;
+    console.log(data[index].definition);
+    rl.prompt();
+    rl.on('line', (line) => {
+        if(line == data[index].term){
+            console.log('Selamat, Anda Benar!\n'); 
+            index++;
+        }else{
+            console.log('wkwk, Anda Kurang Beruntung\n')
+        }
+        
+        if(index == data.length){
+            console.log('Hore Anda Menang!\n')
+            rl.close()
+        }
+        console.log(data[index].definition);
+        rl.prompt();
+    }).on('close', () => {
+        // console.log('Good bye');
+        process.exit(0);
+    });
+});
+
+
