ac739c1306a746ac96f14f89818c5b05d0467d1e
mahyudin akbar <mahyudinakbar10@gmail.com>
2023-03-03 15:24:12 +0700

    C4 edit

--- C4/indexprime.js
+++ C4/indexprime.js
@@ -4,22 +4,21 @@ function indexPrime(param1){
 
     while(prime.length < param1){
         let status = true;
-        for(let i=2; i < number; i++){
+        for(let i=2; i <= Math.sqrt(number); i++){
             if(number % i === 0){
                 status =false;
             }
         }
         if(status === true){
-            prime.push(number)
+            prime.push(number);
         }
-    number++
+    number++;
     }
-// return prime;
 return prime[param1-1];
 }
-console.log(indexPrime(4));
+// console.log(indexPrime(4));
 // console.log(indexPrime(500));
 // console.log(indexPrime(2000));
 // console.log(indexPrime(5000));
-console.log(indexPrime(7000));
-// console.log(indexPrime(37786));
+// console.log(indexPrime(7000));
+console.log(indexPrime(37786));
