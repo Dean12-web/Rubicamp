d60c1c2e4f03c52df3ea50140f88258196ea1e7e
mahyudin akbar <mahyudinakbar10@gmail.com>
2023-03-04 10:34:39 +0700

    Change into arrow function

--- case2/deret.js
+++ case2/deret.js
@@ -1,12 +1,12 @@
-function deretKaskus(n) {
-    //write code here
+const deretKaskus = (n) => {
     let arr = []
-    for (i = 3; i <= n * 3; i+= 3){
-        if((i % 5 == 0) && (i % 6 == 0)){
+    let length = n * 3 //efektifitas code (memori)
+    for (i = 3; i <= length; i += 3) {
+        if ((i % 5 == 0) && (i % 6 == 0)) {
             arr.push('KASKUS')
-        }else if (i % 5 == 0) {
+        } else if (i % 5 == 0) {
             arr.push("KAS")
-        }else if(i % 6 == 0){
+        } else if (i % 6 == 0) {
             arr.push('KUS')
         } else {
             arr.push(i);
