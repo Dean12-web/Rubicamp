526808ad1c22143732ef0cde41c3b6d4dfa5e6f7
mahyudin akbar <mahyudinakbar10@gmail.com>
2023-03-05 14:01:35 +0700

    C13 2nd commit

--- C13/daftar.js
+++ C13/daftar.js
@@ -16,11 +16,25 @@ $ node daftar.js list:completed asc|desc
 $ node daftar.js tag <task_id> <tag_name_1> <tag_name_2> ... <tag_name_N>
 $ node daftar.js filter:<tag_name>`
 
+
+const inputNew = [{
+    number : 1,
+    task : "Saya mau pergi lagi"
+},{
+    number : 2,
+    task : "Saya mau pergi"
+}]
+const input = {
+    number : 2,
+    task : "Saya mau pergi"
+}
 let rawdata = fs.readFileSync('data.json');
 let data = JSON.parse(rawdata)
 
+const jsonString = JSON.stringify(inputNew);
+fs.writeFileSync('data.json', jsonString)
 if(args === undefined){
     console.log(daftarList)
 }else{
-    console.log('Saya mau pergi ke pasar!')
+    console.log('Data Added!')
 }
