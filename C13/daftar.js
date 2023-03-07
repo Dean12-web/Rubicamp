709a8be0e533520e776d66becd7327e70c7b9f65
mahyudin akbar <mahyudinakbar10@gmail.com>
2023-03-07 12:54:19 +0700

    C13 add data done

--- C13/daftar.js
+++ C13/daftar.js
@@ -1,9 +1,14 @@
 const fs = require('node:fs');
 const process = require('node:process');
 
-const args2 = process.argv[3];
+const args  = process.argv
 const args1 = process.argv[2];
-// console.log(args);
+const args2 = process.argv[3];
+
+// console.log("number of arguments is "+args.length);
+// for (let i = 0; i < args.length; i++) {
+//     console.log(`${i}: ${args[i]}`)
+// }
 const daftarList = `>>>> JS TODO <<<<
 $ node daftar.js <command>
 $ node daftar.js list
@@ -32,42 +37,50 @@ if(args1 === undefined){
             console.log(`${i+1}. [${data[i].status ? 'X' : ' '}] ${data[i].task_content}`)    
         }
     }else if(args1 === 'task'){
-        // console.log("Daftar Pekerjaan")
+        console.log("Daftar Pekerjaan")
         for (let i = 0; i < data.length; i++){
             if(args2 == data[i].task_id){
                 console.log(`task_id : ${data[i].task_id} [${data[i].status ? 'X' : ' '}] ${data[i].task_content}`)
+            }else if( args2 == undefined){
+                console.log("Masukkan id task")
             }
         }
     }else if(args1 === 'add'){
-        let input = args2;
-        let indexAdd = 1;
-        let task = "Saya akan mencuci"
+        let sentence = ''
+        let index = 1;
         for (let i = 0; i < data.length; i++) {
-            indexAdd++
+            index++;   
+        }
+        for (let i = 3; i < args.length; i++) {
+            sentence += args[i] + ' ';
         }
         data.push({
-            "task_id" : indexAdd,
-            "task_content" : task,
-            "status" : false, 
+            "task_id" : index,
+            "task_content" : sentence.trim(),
+            "status" : false,
+            "tags"   : []
         })
         fs.writeFileSync('data.json', JSON.stringify(data,null,4))
-        console.log(`"${task}" telah di tambahkan`)
+        console.log(`"${sentence}" telah di tambahkan`)
     }else if(args1 === 'delete'){
-        data.splice(data,1);
-        fs.writeFileSync('data.json', JSON.stringify(data, null, 4))
-        // console.log(`${task_id} telah di hapus dari daftar`)
+        const deleteData = data.task_content;
+        data.splice(parseInt(args2) - 1, 1);
+        fs.writeFileSync('data.json', JSON.stringify(data,null,4))
+        console.log(`${deleteData} telah di hapus dari daftar`)
     }else if(args1 === 'complete'){
         console.log("data ke i telah selesai")
     }else if(args1 === 'uncompleted'){
         console.log("data ke i belum selesai")
     }else if(args1 === 'list:outstanding'){
         if(args2 == 'asc'){
+            console.log("Daftar Pekerjaan")
             for (let i = 0; i < data.length; i++) {
                 if(data[i].status === false){
                     console.log(`${i+1}.[ ] ${data[i].task_content}`)    
                 }   
             }
         }else{
+            console.log("Daftar Pekerjaan")
             for (let j = data.length -1 ; j >= 0; j--) {
                 if(data[j].status === false){
                     console.log(`${j+1}.[ ] ${data[j].task_content}`)    
@@ -76,21 +89,28 @@ if(args1 === undefined){
         }
     }else if(args1 === 'list:completed'){
         if(args2 == 'asc'){
+            console.log("Daftar Pekerjaan")
             for (let i = 0; i < data.length; i++) {
                 if(data[i].status === true){
                     console.log(`${i+0}.[X] ${data[i].task_content}`)    
                 }
             }
         }else{
+            console.log("Daftar Pekerjaan")
             for (let j = data.length -1 ; j >= 0; j--) {
                 if(data[j].status === true){
                     console.log(`${j+0}.[X] ${data[j].task_content}`)    
                 }
             }
         }
-    }else if(args1 === 'filter'){
-        console.log("data ke i  yg difilter")
+    }else if(args1 === 'tags'){
+        let inputId = args2;
+        let inputTag = process.argv[4];
+        data.push({
+            "tags" : inputTag,
+        })
+        fs.writeFileSync('data.json', JSON.stringify(data, null, 4));
     }else{
-        console.log("tag data")
+        console.log("Filter Data")
     }
 }
