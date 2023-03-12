52d6ebf02d4bdb1bd401565c48e45805d7ccdb21
mahyudin akbar <mahyudinakbar10@gmail.com>
2023-03-12 15:04:20 +0700

    C15 First Commit

---
+++ C16/simple.js
@@ -0,0 +1,30 @@
+class Tyre {
+    constructor(brand, size){
+
+    }
+}
+
+class Car {
+
+}
+
+class CarFactory {
+    constructor() {
+        this.cars = []
+    }
+
+    produce(year) {
+
+    }
+
+    guaranteeSimulation(simulationYear) {
+
+    }
+}
+
+const toyota = new CarFactory()
+toyota.produce(2020)
+toyota.produce(2020)
+toyota.result()
+toyota.guaranteeSimulation(2025)
+
