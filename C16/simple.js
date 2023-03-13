5e2e92b01e7193de555ddb322586730859084b90
mahyudin akbar <mahyudinakbar10@gmail.com>
2023-03-13 23:14:32 +0700

    C16 5th commit

--- C16/simple.js
+++ C16/simple.js
@@ -1,50 +1,57 @@
 class CarFactory {
-    constructor() {
+    constructor () {
         this.cars = []
     }
     produce(year) {
-        let car = [];
-        let makeAgya = new Agya()
-        car.push(makeAgya.produceAgya());
-    console.log(car)
-    }
-    
-    guaranteeSimulation(simulationYear){
-
+        const makeAgya = new Agya(year);
+        this.cars.push(makeAgya.produceAgya())
+        console.log(this.cars)
     }
 }
 
 class Car {
-    constructor(varian, serialNumber, door, seat, year, brand, size) {
-        this.newVarian = varian
-        this.newSerialNumber = serialNumber
-        this.newDoor = door 
-        this.newSeat = seat
-        this.newTyre = new Tyre(brand,size)
-        this.newYear = year
-        this.newWarranty = 2025
+    constructor(year, seat, door, brand, size) {
+        this.seatProduce = seat,
+        this.yearProduce = year,
+        this.doorProduce = door
+        this.tyreProduce = new Tyre(brand,size)
     }
-
-    varian() {
-        return this.newVarian;
-    }
-    serialNumber (){
-        return this.newSerialNumber;
-    }
-    door() {
-        return this.newDoor;   
+    serialNumber() {
+        const chars = '1234567890abcdefghijklmnopqrstuvwxyz';
+        const serialLength = [7,4,4,12];
+        let randomSerial = "", randomSerial2 = "", randomSerial3 = "",randomSerial4= ""
+        let randomNumber
+        for (let i = 0; i < serialLength[0]; i++) {
+            randomNumber = Math.floor(Math.random() * chars.length);
+            randomSerial += chars.substring(randomNumber, randomNumber + 1);
+        }
+        for (let i = 0; i < serialLength[1]; i++) {
+            randomNumber = Math.floor(Math.random() * chars.length);
+            randomSerial2 += chars.substring(randomNumber, randomNumber + 1);
+        }
+        for (let i = 0; i < serialLength[2]; i++) {
+            randomNumber = Math.floor(Math.random() * chars.length);
+            randomSerial3 += chars.substring(randomNumber, randomNumber + 1);
+        }
+        for (let i = 0; i < serialLength[3]; i++) {
+            randomNumber = Math.floor(Math.random() * chars.length);
+            randomSerial4 += chars.substring(randomNumber, randomNumber + 1);
+        }  
+        return `${randomSerial}-${randomSerial2}-${randomSerial3}-${randomSerial4}`
     }
     seat() {
-        return this.newSeat;
+        this.seatProduce = 5
+        return this.seatProduce;
+    }
+    year() {
+        return this.yearProduce;
+    }
+    door() {
+        this.doorProduce = 4
+        return this.doorProduce;
     }
     tyre() {
-        return this.newTyre;
-    }  
-    year() {
-        return this.newYear;   
-    }
-    warranty() {
-        return this.newWarranty;
+        return this.tyreProduce.brand();
     }
 }
 
@@ -55,59 +62,25 @@ class Tyre {
         size = [15,17]
         this._size = size[Math.floor(Math.random() * 2)]
     }
+    brand(){
+        return `${this._brand} ${this._size} inch`
+    }
 }
 
-class Agya extends Car{
-    produceAgya(){
-        let obj = {
-            varian   : "Agya",
-            sn       : "randomsn",
-            door     : 5,
-            seat     : `5 seater`,
-            tyre     : `dunlop 15 inch`,
-            year     : 2020,
-            warranty : `1 year`
+class Agya extends Car {
+    produceAgya() {
+        const obj = {
+            varian      : "Agya",
+            sn          : this.serialNumber(),
+            door        : this.door(),
+            seat        : `${this.seat()} seater`,
+            tyre        : this.tyre(),
+            year        : this.year(),
+            warranty    : '1 year'
         }
-        return obj;
+        return obj
     }
 }
 
-let toyota = new CarFactory();
-toyota.produce()
-function randomSn() {
-    var chars = '1234567890abcdefghijklmnopqrstuvwxyz',
-    serialLength = 7,
-    randomSerial = "",
-    i,
-    randomNumber;
-    for (i = 0; i < serialLength; i = i + 1) {
-        randomNumber = Math.floor(Math.random() * chars.length);
-        randomSerial += chars.substring(randomNumber, randomNumber + 1);
-    }
-    var serialLength1 = 4,
-    randomSerial1 = ""
-    for (i = 0; i < serialLength1; i = i + 1) {
-        randomNumber = Math.floor(Math.random() * chars.length);
-        randomSerial1 += chars.substring(randomNumber, randomNumber + 1);
-    }
-    var serialLength2 = 4,
-    randomSerial2 = ""
-    for (i = 0; i < serialLength2; i = i + 1) {
-        randomNumber = Math.floor(Math.random() * chars.length);
-        randomSerial2 += chars.substring(randomNumber, randomNumber + 1);
-    }
-    var serialLength3 = 4,
-    randomSerial3 = ""
-    for (i = 0; i < serialLength3; i = i + 1) {
-        randomNumber = Math.floor(Math.random() * chars.length);
-        randomSerial3 += chars.substring(randomNumber, randomNumber + 1);
-    }
-    var serialLength4 = 12,
-    randomSerial4 = ""
-    for (i = 0; i < serialLength4; i = i + 1) {
-        randomNumber = Math.floor(Math.random() * chars.length);
-        randomSerial4 += chars.substring(randomNumber, randomNumber + 1);
-    }
-    return `${randomSerial}-${randomSerial1}-${randomSerial2}-${randomSerial3}-${randomSerial4}  `;
-}
-
+const toyota = new CarFactory()
+toyota.produce(2020)
