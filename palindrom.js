4ea4188ecdf7efd51a73635a50f13b5b8d19f4bf
Dean12 <mahyudinakbar10@gmail.com>
2024-08-05 13:57:29 +0700

    coba

--- palindrom.js
+++ palindrom.js
@@ -1,4 +1,5 @@
 function isPalindrom(str) {
+    str = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
     let length = str.length - 1;
     for (let i = 0; i < length / 2 ; i++) {
         if (str[i] != str[length]) {
@@ -9,6 +10,4 @@ function isPalindrom(str) {
     return true;
 }
 
-console.log(isPalindrom('nababan'))
-
-console.log(isPalindrom('nam'))
+console.log(isPalindrom('ibu ratna antar ubi'))
